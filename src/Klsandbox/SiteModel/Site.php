2ab51ac338b3373c2af88757fcf5fe580fc57d0f
Ibrahim Abdul Rahim <ibrahim@cara.com.my>
2015-06-10 11:32:32 +0800

    first

---
+++ src/Klsandbox/SiteModel/Site.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App;
+use Config;
+
+/**
+ * App\Models\Site
+ *
+ * @property integer $id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $key
+ * @property string $name
+ * @property string $description
+ * @property string $host
+ * @property string $status
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereKey($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereHost($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Site whereStatus($value)
+ */
+class Site extends Model {
+
+    //
+    protected $fillable = ['key', 'name', 'description', 'status', 'host'];
+    private static $siteId;
+    private static $siteKey;
+
+    public static function setSite($site) {
+        self::$siteId = $site->id;
+        self::$siteKey = $site->key;
+    }
+
+    public static function setSiteByHost($host) {
+        $site = Site::firstByAttributes(['host' => $host]);
+        if ($site) {
+            self::setSite($site);
+        }
+    }
+
+    public static function DevSite() {
+        $site = Site::firstByAttributes(['Status' => 'Dev']);
+        return $site;
+    }
+
+    public static function ProdSite() {
+        $site = Site::firstByAttributes(['Status' => 'Production']);
+        return $site;
+    }
+
+    public static function id() {
+        if (self::$siteId) {
+            return self::$siteId;
+        }
+
+        return Config::get('site.fallback_id');
+    }
+
+    public static function key() {
+        if (self::$siteKey) {
+            return self::$siteKey;
+        }
+
+        return Config::get('site.fallback_key');
+    }
+
+    public static function protect($item, $label = "Item") {
+        if (!$item) {
+            App::abort(404, "$label not found.");
+        }
+
+        if ($item->site_id != Site::id()) {
+            App::abort(403, 'Unauthorized action.');
+        }
+    }
+
+}
---
+++ src/Klsandbox/SiteModel/SiteModelServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Klsandbox\SiteModel;
+
+use Illuminate\Support\ServiceProvider;
+
+class SiteModelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
