eed9889cf7e097b42f20c07f50519e9b4b893e3e
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2015-06-22 01:18:33 -0700

    site extensions

---
+++ src/Klsandbox/SiteModel/SiteExtensions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Klsandbox\SiteModel;
+
+trait SiteExtensions {
+    
+    public static function forSite()
+    {
+        return self::where('site_id', '=', Site::id());
+    }
+
+}
