28df0b57e6a763a46184c048006555b798363ed3
Klsandbox <ibrahim@klsandbox.com>
2016-05-13 11:33:35 +0800

    set-site

---
+++ src/Klsandbox/SiteModel/Middleware/SetSite.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Klsandbox\SiteModel\Middleware;
+
+use Closure;
+use Klsandbox\SiteModel\Site;
+
+class SetSite
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $host = $request->getHost();
+        $site = Site::setSiteByHost($host);
+
+        $response = $next($request);
+        return $response;
+    }
+}
